a14986fc3ad1fa6ed0aeca5ceb731067e1c9b091
ZanOFFC <sepenpreset@gmail.com>
2025-03-29 19:09:00 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/main.css';
 
+// Gunakan createRoot untuk React 18
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
